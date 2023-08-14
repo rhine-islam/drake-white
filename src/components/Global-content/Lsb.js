3ab20531ef6005f598aaa6f4c0c653bb294bb715
Rhine <rhine.cse@gmail.com>
2023-08-14 19:40:34 +0600

    ⚒️ Fix : added hover effect

--- src/components/Global-content/Lsb.js
+++ src/components/Global-content/Lsb.js
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from "react";
 import data from "../../data";
 import { CopyToClipboard } from "react-copy-to-clipboard";
+import LogoLight from "../LogoLight";
+import Facebook from "../SVG/Facebook";
+import Linkedin from "../SVG/Linkedin";
+import Instagram from "../SVG/Instagram";
+import Whatsapp from "../SVG/Whatsapp";
 
 export default function Lsb() {
   const [isMobile, setIsMobile] = useState(false);
@@ -38,38 +43,50 @@ export default function Lsb() {
         {data.countryOfResidence} , {data.cityOfResidence}
       </h2>
 
-      <ul class="flex-container space-between">
-        <i className="las la-envelope flex-item svgIcon"></i>
-        <CopyToClipboard text={data.email} onCopy={() => setCopied(true)}>
-          <li className={`flex-item ${copied && "primaryColor"}`}>
-            {isMobile === true ? `${data.email.slice(0, 18)}...` : data.email}
+      <div className="social-media">
+        <Facebook />
+        <Linkedin />
+        <Instagram />
+        <Whatsapp />
+      </div>
+      <div className="grid">
+        <ul class="flex-container space-between">
+          <i className="las la-envelope flex-item svgIcon"></i>
+          <CopyToClipboard text={data.email} onCopy={() => setCopied(true)}>
+            <li className={`flex-item ${copied && "primaryColor"}`}>
+              {isMobile === true ? `${data.email.slice(0, 18)}...` : data.email}
+            </li>
+          </CopyToClipboard>
+          <i className="las la-angle-right flex-item svgIcon"></i>
+        </ul>
+        <div style={{ borderBottom: "0.4px solid black" }} />
+        <ul class="flex-container space-between">
+          <i className="las la-phone flex-item svgIcon"></i>
+          <li className="flex-item">
+            {data.countryCode}
+            {data.personalNunber}
           </li>
-        </CopyToClipboard>
-        <i className="las la-angle-right flex-item svgIcon"></i>
-      </ul>
-      <ul class="flex-container space-between">
-        <i className="las la-phone flex-item svgIcon"></i>
-        <li className="flex-item">
-          {data.countryCode}
-          {data.personalNunber}
-        </li>
-        <i className="las la-angle-right flex-item svgIcon"></i>
-      </ul>
-      <ul class="flex-container space-between">
-        <i className="las la-phone flex-item svgIcon"></i>
-        <li className="flex-item">
-          {data.countryCode}
-          {data.personalNunber}
-        </li>
-        <i className="las la-angle-right flex-item svgIcon"></i>
-      </ul>
-      <ul class="flex-container space-between">
-        <i className="las la-basketball-ball flex-item svgIcon"></i>
-        <li className="flex-item">
-          {data.premanentAddress && `${data.premanentAddress.slice(0, 28)}...`}
-        </li>
-        <i className="las la-angle-right flex-item svgIcon"></i>
-      </ul>
+          <i className="las la-angle-right flex-item svgIcon"></i>
+        </ul>
+        <div style={{ borderBottom: "0.4px solid black" }} />
+        <ul class="flex-container space-between">
+          <i className="las la-phone flex-item svgIcon"></i>
+          <li className="flex-item">
+            {data.countryCode}
+            {data.personalNunber}
+          </li>
+          <i className="las la-angle-right flex-item svgIcon"></i>
+        </ul>
+        <div style={{ borderBottom: "0.4px solid black" }} />
+        <ul class="flex-container space-between">
+          <i className="las la-basketball-ball flex-item svgIcon"></i>
+          <li className="flex-item">
+            {data.premanentAddress &&
+              `${data.premanentAddress.slice(0, 28)}...`}
+          </li>
+          <i className="las la-angle-right flex-item svgIcon"></i>
+        </ul>
+      </div>
       <a href="#" className="theme-btn" style={{ margin: "10px 0px 5px 0px" }}>
         <i className="las la-envelope"></i> Connect
       </a>
---
+++ src/components/LogoLight/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const LogoLight = () => {
+  return (
+    <svg
+      width="46"
+      height="40"
+      viewBox="0 0 46 40"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={`relative w-8 h-8 md:h-10 md:w-10 `}
+    >
+      <path
+        d="M0.97561 20C0.97561 9.50371 9.68706 0.97561 20.4571 0.97561C29.004 0.97561 36.2544 6.34632 38.8835 13.8093C38.9541 14.0096 39.1421 14.1463 39.3545 14.1463C39.6867 14.1463 39.9235 13.8238 39.8135 13.5102C37.0557 5.64531 29.4248 0 20.4571 0C9.16967 0 0 8.94372 0 20C0 31.0563 9.16967 40 20.4571 40C29.4248 40 37.0557 34.3547 39.8135 26.4898C39.9235 26.1762 39.6867 25.8537 39.3545 25.8537C39.1421 25.8537 38.9541 25.9904 38.8835 26.1907C36.2544 33.6537 29.004 39.0244 20.4571 39.0244C9.68706 39.0244 0.97561 30.4963 0.97561 20Z"
+        fill="white"
+      />
+      <path
+        d="M20 20H44.238"
+        stroke="url(#paint0_linear_521_5195)"
+        stroke-width="2"
+        stroke-linecap="round"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_521_5195"
+          x1="20"
+          y1="20.5106"
+          x2="43.914"
+          y2="20.5106"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#7C6FF9" />
+          <stop offset="1" stop-color="#6FEFFC" stop-opacity="0.99" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
+
+export default LogoLight;
